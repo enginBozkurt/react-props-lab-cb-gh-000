ccb3e7561a3e5ec4f3eca1ad9b875c05bb7e68cc
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-24 12:09:51 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -2,3 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Spaceship extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>{this.props.name}</h1>
+          <h2>{this.props.speed}</h2>
+          <h2>{this.props.hasRockets}</h2>
+          <h2>{this.props.colors.join(', ')}</h2>
+        </div>
+    )
+  } 
+}
+
